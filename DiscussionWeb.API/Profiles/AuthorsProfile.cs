184bbd14f1a749ef8538f0dae3bf045b59b754b2
KUTlime <kutlime@gmail.com>
2020-06-21 10:39:41 +0200

    Initial boiler-plate code generation.

---
+++ DiscussionWeb.API/Profiles/AuthorsProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+namespace DiscussionWeb.API.Profiles
+{
+	internal class AuthorsProfile : Profile
+	{
+	}
+
+	internal class DiscussionThread : Profile
+	{
+
+	}
+
+	internal class Posts : Profile
+	{
+
+	}
+}
