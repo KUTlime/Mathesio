8c7f5de6886ab92648f936df241bbfc4cd7c04d9
KUTlime <kutlime@gmail.com>
2020-06-22 10:39:01 +0200

    Update IRepository interface.

--- DiscussionWeb.Data/Services/IDiscussionWebRepository.cs
+++ DiscussionWeb.Data/Services/IDiscussionWebRepository.cs
@@ -9,17 +9,31 @@ namespace DiscussionWeb.Data.Services
 	public interface IDiscussionWebRepository
 	{
 		void AddPost(Guid authorId, Post post);
+
 		void DeletePost(Post post);
+
 		Post GetPost(Guid authorId, Guid postId);
+
 		IEnumerable<Post> GetPosts(Guid authorId);
+
 		void UpdatePost(Post post);
+
 		void AddAuthor(Author author);
+
 		bool AuthorExists(Guid authorId);
+
 		void DeleteAuthor(Author author);
+
 		Author GetAuthor(Guid authorId);
+
 		IEnumerable<Author> GetAuthors();
+
 		IEnumerable<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters);
+
 		IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+
 		void UpdateAuthor(Author author);
+
+		bool Save();
 	}
 }
